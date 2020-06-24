b0d8605f28e918e893a2537e02cb40553119bae2
Jack Milton <originalmiltonz@gmail.com>
2020-06-24 19:04:02 +0100

    set up basic structure of header.js and imported it to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 //Components import
 import Form from './components/form/form';
+import Header from './components/header/header';
 
 const App = () => (
   <>
     <h1>Hello, World!</h1>
     <Form />
+    <Header />
   </>
 );
 
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+
+    return (
+        <>
+            <div>
+                <h2>The</h2>
+                <h1>FottyNamePicker</h1>
+            </div>
+        </>
+    );
+}
+
+export default Header;
