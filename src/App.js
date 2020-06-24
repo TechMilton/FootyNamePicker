2e96aa809a498bb2e4283be93c564f5063bff149
Jack Milton <originalmiltonz@gmail.com>
2020-06-24 19:29:45 +0100

    Added button component.js and imported it to App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Form from './components/form/form';
 //Header related components 
 import Header from './components/header/header';
 import EnterName from './components/header/enterName';
+import Button from './components/form/button';
 
 const App = () => (
   <>
+
     <Header />
     <EnterName />
+
     <Form />
+    <Button />
   </>
 );
 
---
+++ src/components/form/button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <>
+            <div>
+                <button className="main-btn" type="submit">Add to list</button>
+            </div>
+        </>
+    );
+}
+
+export default Button;
