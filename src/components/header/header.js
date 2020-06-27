acd991f41847c96c16aaf9585fa064d157c517bb
Jack Milton <originalmiltonz@gmail.com>
2020-06-27 23:53:49 +0100

    Fixed title spelling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <h2 className="page-header__the">The</h2>
-            <h1 className="page-header__title">FottyTeamPicker</h1>
+            <h1 className="page-header__title">FootyTeamPicker</h1>
         </>
     );
 }
