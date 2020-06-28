710d61625bc77d630b8342005d924a8198b0a081
Jack Milton <originalmiltonz@gmail.com>
2020-06-28 14:54:11 +0100

    Updates

---
+++ precache-manifest.503080d111e2ed6107b196d737bc0eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1474062cfe1ee5d37cd6be26d59a8942",
+    "url": "/FootyNamePicker/index.html"
+  },
+  {
+    "revision": "c1b4171ff726e8885730",
+    "url": "/FootyNamePicker/static/css/main.7f59116c.chunk.css"
+  },
+  {
+    "revision": "1151d1b977d7757721ba",
+    "url": "/FootyNamePicker/static/js/2.210f6ab3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FootyNamePicker/static/js/2.210f6ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b4171ff726e8885730",
+    "url": "/FootyNamePicker/static/js/main.fcdb0805.chunk.js"
+  },
+  {
+    "revision": "d57b781f6c148371d6a9",
+    "url": "/FootyNamePicker/static/js/runtime-main.3c79c773.js"
+  }
+]);
