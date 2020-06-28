7b5747ec4e0545521841c09169a3d7e8b596140e
Jack Milton <originalmiltonz@gmail.com>
2020-06-28 20:43:08 +0100

    Updates

--- precache-manifest.097e95c949ff0b00bed349f4b30b0f90.js
+++ precache-manifest.097e95c949ff0b00bed349f4b30b0f90.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "274853e8fbf1f6c0483f38936b033794",
+    "revision": "11c28869672e8175c2e64fecfd8d328c",
     "url": "/FootyNamePicker/index.html"
   },
   {
-    "revision": "7ab4c117cb218eb08a6e",
+    "revision": "e92155eac87add2d356f",
     "url": "/FootyNamePicker/static/css/main.7f59116c.chunk.css"
   },
   {
-    "revision": "3b7e8a1afe6b3ea256e0",
-    "url": "/FootyNamePicker/static/js/2.d3fd8371.chunk.js"
+    "revision": "1151d1b977d7757721ba",
+    "url": "/FootyNamePicker/static/js/2.210f6ab3.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/FootyNamePicker/static/js/2.d3fd8371.chunk.js.LICENSE.txt"
+    "url": "/FootyNamePicker/static/js/2.210f6ab3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7ab4c117cb218eb08a6e",
-    "url": "/FootyNamePicker/static/js/main.5510c7f9.chunk.js"
+    "revision": "e92155eac87add2d356f",
+    "url": "/FootyNamePicker/static/js/main.45ab5296.chunk.js"
   },
   {
     "revision": "d57b781f6c148371d6a9",
