07861f8e514f7659f2d3f27e1e78e0637862edc5
Jack Milton <originalmiltonz@gmail.com>
2020-06-28 20:21:32 +0100

    Updates

---
+++ precache-manifest.84111f026bde80dcab7407e9118736fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8488f32cca742e146328a0799cf2e62a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab4c117cb218eb08a6e",
+    "url": "/static/css/main.7f59116c.chunk.css"
+  },
+  {
+    "revision": "3b7e8a1afe6b3ea256e0",
+    "url": "/static/js/2.d3fd8371.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3fd8371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab4c117cb218eb08a6e",
+    "url": "/static/js/main.5510c7f9.chunk.js"
+  },
+  {
+    "revision": "909983d941692ee5f167",
+    "url": "/static/js/runtime-main.0ed96b0b.js"
+  }
+]);
