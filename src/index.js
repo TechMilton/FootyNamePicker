264776475dc0e5cb4cbcf174656cf628e4c1939a
Jack Milton <originalmiltonz@gmail.com>
2020-06-28 21:25:50 +0100

    Fixing router problems

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 console.log('index.js is running')
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>
+  </HashRouter>
   , document.getElementById('root'));
 
